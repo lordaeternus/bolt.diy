d4bc2c31c43184b99552d6a65f7b07bb59067122
lordaeternus <32939264+lordaeternus@users.noreply.github.com>
2025-06-29 16:32:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
+dotenv.config()
+
 
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: 'all',
+    allowedHosts: process.env.ALLOWED_HOSTS
+      ? process.env.ALLOWED_HOSTS.split(',').map(h => h.trim())
+      : 'all',
     strictPort: true
   }
 })
