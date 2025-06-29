07404082494a167c740f808d84f4c166ff722c64
lordaeternus <32939264+lordaeternus@users.noreply.github.com>
2025-06-29 16:23:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-
 export default defineConfig({
   server: {
-    host: true, // necessário para aceitar conexões externas
-    port: 3000, // ou a porta que o easypanel exige
-    allowedHosts: ['automacao-boltdiy.0rau8r.easypanel.host'],
-    strictPort: false // evita conflitos se a porta estiver ocupada
+    host: true,
+    port: 3000,
+    allowedHosts: 'all' // desabilita o filtro de hosts
   }
 });
