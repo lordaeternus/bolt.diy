841a9f4994af5925793423a9e83a5eb153d16b7c
lordaeternus <32939264+lordaeternus@users.noreply.github.com>
2025-07-01 15:32:32 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    host: '0.0.0.0', // permite acesso externo
-    allowedHosts: ['automacao-boltdiy.0rau8r.easypanel.host'] // seu domínio
+    host: '0.0.0.0',
+    allowedHosts: ['automacao-boltdiy.0rau8r.easypanel.host']
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['automacao-boltdiy.0rau8r.easypanel.host']
   }
 });
