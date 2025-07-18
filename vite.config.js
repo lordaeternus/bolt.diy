e0088f525d1433c011684a322efbf51c2396dd57
lordaeternus <32939264+lordaeternus@users.noreply.github.com>
2025-07-18 18:19:37 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['automacao-boltdiy.0rau8r.easypanel.host']
-  },
-  preview: {
-    host: '0.0.0.0',
-    allowedHosts: ['automacao-boltdiy.0rau8r.easypanel.host']
+  plugins: [react()],
+  server: { // Adicione esta seção
+    allowedHosts: [
+      'automacao-boltdiy.0rau8r.easypanel.host'
+    ]
   }
-});
+})
